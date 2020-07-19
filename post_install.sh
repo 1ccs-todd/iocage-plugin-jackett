1ff60ee3b8bdf1de6ef33fe249f114f76d3414f8
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-19 21:37:59 +0200

    Initial merge with jailman scripts

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,19 @@
 # this is needed for update to work properly
 chown -R jackett /usr/local/share/jackett
 
-# Enable and start service
-sysrc jackett_enable=YES
-service jackett start
+"fetch  https://github.com/jailmanager/jailmanager.github.io/releases/download/v0.0.1/mono-6.8.0.105.txz -o /usr/local/share"
+pkg install -y /usr/local/share/mono-6.8.0.105.txz
+rm /usr/local/share/mono-6.8.0.105.txz
+
+# Initialise defaults
+FILE_NAME=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | jq -r ".assets[] | select(.name | contains(\"Mono.tar.gz\")) | .name")
+DOWNLOAD=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | jq -r ".assets[] | select(.name | contains(\"Mono.tar.gz\")) | .browser_download_url")
+
+fetch -o /usr/local/share "${DOWNLOAD}"
+"tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share"
+rm /usr/local/share/"${FILE_NAME}"
+"pw user add jackett -c jackett -u 818 -d /nonexistent -s /usr/bin/nologin"
+
+
+chmod u+x /usr/local/etc/rc.d/jackett
+sysrc "jackett_enable=YES"
