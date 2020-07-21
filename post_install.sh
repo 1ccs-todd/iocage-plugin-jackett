d08505129e5fec195dba3196f8dcbbdb0fc0ab22
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-21 18:09:38 +0200

    try bash again

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/local/bin/bash
 pw user add jackett -c jackett -u 818 -d /nonexistent -s /usr/bin/nologin
 
 fetch  https://github.com/jailmanager/jailmanager.github.io/releases/download/v0.0.1/mono-6.8.0.105.txz -o /usr/local/share
@@ -12,8 +11,6 @@ DOWNLOAD=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest 
 
 fetch -o /usr/local/share "${DOWNLOAD}"
 tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share
-# Need to make jackett user own /usr/local/share/jackett folder
-# this is needed for update to work properly
 rm /usr/local/share/"${FILE_NAME}"
 
 chmod u+x /usr/local/etc/rc.d/jackett
